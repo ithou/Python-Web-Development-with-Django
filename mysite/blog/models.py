594d3526731ee01bc7741ad828193a01e67cb424
ithou <me@ithee.cn>
2018-12-25 21:33:38 +0800

    前两章完

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -7,7 +7,10 @@ class BlogPost(models.Model):
     """文章"""
     title = models.CharField(max_length=150)
     body = models.TextField()
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField()  # 时间戳
 
     """严格来说有四个变量，Django会默认为model加上一个id"""
+
+    class Meta:
+        ordering = ['-timestamp']
 
